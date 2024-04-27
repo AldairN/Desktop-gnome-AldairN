798e5edf6055e4ecce2d8c9cdf7a96d59d7733b6
AldairN <157073386+AldairN@users.noreply.github.com>
2024-04-27 13:52:48 -0600

    Delete conky/grumicela directory

--- conky/grumicela/start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-killall conky
-sleep 2s
-		
-conky -c $HOME/.config/conky/grumicela/Celaeno.conf &> /dev/null &
-conky -c $HOME/.config/conky/grumicela/Grumium2.conf &> /dev/null &
-
-exit
